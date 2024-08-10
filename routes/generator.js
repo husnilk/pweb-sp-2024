1dbd8e2aabfa7de4116b42a250749a6eba7ef322
husnilk <husnil.k@gmail.com>
2024-08-10 13:16:38 +0700

    Added: Excel Generator

--- routes/generator.js
+++ routes/generator.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const db = require("../models/index");
 const router = express.Router();
 const PizZip = require("pizzip");
 const Docxtemplater = require("docxtemplater");
 const fs = require("fs");
 const path = require("path");
 const topdf = require("docx2pdf-converter");
+const exceljs = require("exceljs");
 
 // POST /generator/cv
 router.post("/cv", async function (req, res, next) {
@@ -38,6 +40,42 @@ router.post("/cv", async function (req, res, next) {
   topdf.convert(inputPath, outputPath);
 
   res.download(outputPath);
+});
+
+router.get("/templatelist", async function (req, res, next) {
+  const templates = await db.OutgoingType.findAll();
+
+  const workbook = new exceljs.Workbook();
+  const worksheet = workbook.addWorksheet("Templates");
+
+  worksheet.columns = [
+    { header: "Nama Kategori", key: "name", width: 40 },
+    { header: "Bahasa", key: "lang", width: 20 },
+    { header: "Status", key: "status", width: 20 },
+    { header: "Template", key: "template", width: 20 },
+  ];
+
+  for (let i = 0; i < templates.length; i++) {
+    let data = {
+      name: templates[i].name,
+      lang: templates[i].lang,
+      status: templates[i].status,
+      template: templates[i].generated,
+    };
+    worksheet.addRow(data);
+  }
+
+  res.setHeader(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  );
+  res.setHeader(
+    "Content-Disposition",
+    "attachment; filename=" + "templates.xlsx"
+  );
+  workbook.xlsx.write(res).then(function () {
+    res.end();
+  });
 });
 
 module.exports = router;
